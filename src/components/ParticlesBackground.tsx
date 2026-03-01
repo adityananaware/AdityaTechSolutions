f80746e4ca970015de41d412f913e7e7f31eaf07
Aditya Nanaware <adityananaware2@gmail.com>
2026-03-01 19:16:22 +0530

    Update ParticlesBackground.tsx

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -50,7 +50,7 @@ export const ParticlesBackground = () => {
             color: "#2563eb",
             distance: 150,
             enable: true,
-            opacity: 0.3,
+            opacity: 0.2,
             width: 1,
           },
           move: {
