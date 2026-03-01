e8ba954d7e56c2e7bb18b3bd094379c1fd076295
Aditya Nanaware <adityananaware2@gmail.com>
2026-03-01 18:58:04 +0530

    Update ChatWidget.tsx

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -30,7 +30,7 @@ export const ChatWidget = () => {
     try{
 
       const res=await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+API_KEY,
+      ""https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key="+API_KEY,
       {
         method:"POST",
         headers:{
